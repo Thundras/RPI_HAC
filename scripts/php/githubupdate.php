ed49e10b9d5707b4f675bbcf33d245ec1dfbf270
Thundras <ipharao@gmail.com>
2015-02-05 19:55:30 +0100

    Update githubupdate.php

--- scripts/php/githubupdate.php
+++ scripts/php/githubupdate.php
@@ -1,5 +1,11 @@
 <?php 
-`git clean -df`;
-`git reset --hard`;
-`git pull https://github.com/Thundras/RPI_HAC master`;
+$output = shell_exec('git clean -df');
+echo "<pre>$output</pre>";
+$output = shell_exec('git reset --hard');
+echo "<pre>$output</pre>";
+$output = shell_exec('git pull https://github.com/Thundras/RPI_HAC master');
+echo "<pre>$output</pre>";
+//`git clean -df`;
+//`git reset --hard`;
+//`git pull https://github.com/Thundras/RPI_HAC master`;
 ?>
