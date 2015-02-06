a4c723d36cf41840a5ba9963678a6007a5f88797
Thundras <ipharao@gmail.com>
2015-02-06 11:21:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-  echo "<html>\n";
-  echo "\t<head>\n";
-  echo "\t</head>\n";
-  echo "\t<body>\n";
-  echo "\tABCDEFGH\n";
-  echo "\t</body>\n";
-  echo "</html>\n";
+  echo '<html>\n';
+  echo '\t<head>\n';
+  echo '\t\t<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>\n';
+	echo '\t\t<script src="scripts/js/scripts.js" type="text/javascript"></script>\n';
+  echo '\t</head>\n';
+  echo '\t<body>\n';
+  echo '\tABCDEFGH\n';
+  echo '\t</body>\n';
+  echo '</html>\n';
 ?>
