591244dd93d7f195940d51ea328201fa1e1c4ac1
Thundras <ipharao@gmail.com>
2015-02-06 11:53:34 +0100

    Update update.php

--- scripts/php/update.php
+++ scripts/php/update.php
@@ -1,3 +1,3 @@
 <?php
-echo "<div align=center style='background-color:#FFCCCC' width=100%>New Update</div>"
+echo ""; //<div align=center style='background-color:#FFCCCC' width=100%>New Update</div>"
 ?>
