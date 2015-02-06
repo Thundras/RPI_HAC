e7e6b83b5b5f5aee00ca76075df07299128e059a
Thundras <ipharao@gmail.com>
2015-02-06 11:29:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   echo "\t\t<script src='scripts/js/scripts.js' type='text/javascript'></script>\n";
   echo "\t</head>\n";
   echo "\t<body>\n";
-  echo "\tABCDEFGH\n";
+  echo "\t\t<div align=center backcolor=#7FFFFF>New Update</div>\n";
+  echo "\t\t<div>ABCDEFGH</div>\n";
   echo "\t</body>\n";
   echo "</html>\n";
 ?>
