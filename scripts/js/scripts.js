dbd1058bc9923fbb145ad58a3b6941e9dc2d4cb7
Thundras <ipharao@gmail.com>
2015-02-06 11:23:23 +0100

    Create scripts.js

---
+++ scripts/js/scripts.js
@@ -0,0 +1,30 @@
+
+$(document).ready(
+	function()
+	{
+		/* Hier der jQuery-Code */
+		/*
+		alert(jQuery('a').attr('title'));
+		
+		var inhaltUeberschrift = $('h1').html();
+		var inhaltAbsatz = $('p').html();
+		alert($('#absatz1').html());
+		alert($('#absatz1').text());
+
+		$("#DT").load("scripts/time.php");
+		$("#S5").load("scripts/ping.php?host=Samsung-Galaxy-S5&count=1");
+		$("#S3").load("scripts/ping.php?host=Samsung-Galaxy-S3-Neo&count=1");
+		$("#CC").load("scripts/ping.php?host=ChromeNewCastle&count=1");
+		
+		var timeID = setInterval(function(){
+			$("#DT").load('scripts/time.php?' + 1*new Date());
+		}, 1000);
+		
+		var pingID = setInterval(function(){
+			$("#S5").load('scripts/ping.php?host=Samsung-Galaxy-S5&count=5&' + 1*new Date());
+			$("#S3").load('scripts/ping.php?host=Samsung-Galaxy-S3-Neo&count=5&' + 1*new Date());
+			$("#CC").load('scripts/ping.php?host=ChromeNewCastle&count=5&' + 1*new Date());
+		}, 5000);
+		*/
+	}
+);
