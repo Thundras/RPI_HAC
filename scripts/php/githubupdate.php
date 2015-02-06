240fe858d89b3c9348078df9fcf2224332c24893
Thundras <ipharao@gmail.com>
2015-02-06 12:51:07 +0100

    Update githubupdate.php

--- scripts/php/githubupdate.php
+++ scripts/php/githubupdate.php
@@ -1,5 +1,5 @@
 <?php 
-ini_set('max_execution_time', '100');
+ini_set('max_execution_time', '300');
 echo `git clean -df > nul 2>nul &`;
 echo `git reset --hard > nul 2>nul &`;
 echo `git pull origin master > nul 2>nul &`;
