15288c037dad25e9d5bfc62936030935db50acdf
Thundras <ipharao@gmail.com>
2015-02-06 20:58:24 +0100

    Change Update

--- scripts/php/doupdate.php
+++ scripts/php/doupdate.php
@@ -1,10 +1,12 @@
 <?php
+	ob_start();
 	ini_set('max_execution_time', '300');
 	echo "<html>\n";
 	echo "<body>\n";
 	echo "Please wait\n";
 	echo "</body>\n";
 	echo "</html>\n";
+	ob_flush();
 	flush();
 	$tmp = `git clean -df`;
 	$tmp = `git reset --hard`;
