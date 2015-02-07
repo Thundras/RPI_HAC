d1519801fa9388b5d574029211b3c091fa7071c3
Thundras <ipharao@gmail.com>
2015-02-07 10:23:12 +0100

    change index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   echo "\t\t<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>\n";
   echo "\t\t<script src='scripts/js/scripts.js' type='text/javascript'></script>\n";
   echo "\t</head>\n";
-  echo "\t<body style="background-image:url('http://www.designbolts.com/wp-content/uploads/2012/12/Triangle-White-Seamless-Patterns.jpg')>\n";
+  echo "\t<body style=\"background-image:url('http://www.designbolts.com/wp-content/uploads/2012/12/Triangle-White-Seamless-Patterns.jpg');\">\n";
   echo "\t\t<div id='Update' style='text-align:center;' width=100%></div>";
   echo "\t\t<!--<div align=center style='background-color:#FFCCCC' width=100%>New Update</div>-->\n";
   echo "\t\t<div>ABCDEFGHIJK</div>\n";
