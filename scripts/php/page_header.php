22a56f3509eee71cef812548e5a36a5e7332791e
Thundras <ipharao@gmail.com>
2015-02-07 17:04:31 +0100

    change style

--- scripts/php/page_header.php
+++ scripts/php/page_header.php
@@ -1,6 +1,7 @@
 <?php
 	echo "<html>\n";
 	echo "\t<head>\n";
+	echo "\t<link rel='stylesheet' type='text/css' href='css/mystyle.css'>\n";
 	echo "\t\t<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>\n";
 	echo "\t\t<script src='scripts/js/scripts.js' type='text/javascript'></script>\n";
 	echo "\t</head>\n";
