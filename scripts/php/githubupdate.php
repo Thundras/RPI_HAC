8c1c3b34a0c77e2cd4418384d135da5310040023
Thundras <ipharao@gmail.com>
2015-02-07 10:04:42 +0100

    change githubupdate.php

--- scripts/php/githubupdate.php
+++ scripts/php/githubupdate.php
@@ -2,7 +2,7 @@
 $myfile = fopen("update.php", "w") or die("Unable to open file!");
 $txt = "<?php\n";
 fwrite($myfile, $txt);
-$txt = "echo \"<div align=center style='background-color:#FFCCCC' width=100%>New Update<br/><a href='scripts/php/doupdate.php'>Click here for Update</a></div>\n\";";
+$txt = "echo \"<div align=center style='background: rgba(255, 0, 0, 0.2)' width=100%>New Update<br/><a href='scripts/php/doupdate.php'>Click here for Update</a></div>\n\";";
 fwrite($myfile, $txt);
 $txt = "?>\n";
 fwrite($myfile, $txt);
